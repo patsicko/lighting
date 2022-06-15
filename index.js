8a0f94461b5ee44e973d7b311173057dc289887c
patsicko <patsicko@g>
2022-06-15 23:46:54 +0200

    Feat:My commit

---
+++ index.js
@@ -0,0 +1,15 @@
+"use strict";
+// const light=document.getElementById("light");
+// const off=document.getElementById("off");
+// const button=document.createElement("button");
+// button.classList.add("button");
+// button.setAttribute("id","button1");
+// button.setAttribute("type","button");
+// button.createTextNode("Turn the light on");
+// light.appendChild(button);
+// const input=document.createElement("input");
+// input.setAttribute("type","text");
+// light.appendChild(input);
+// button.addEventListener('onclick',function(e){
+//  off.src="on_light.png";
+// })
